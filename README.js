505f06d984bc638d1ec2c70752939dac719caaa2
Alexander Danielov Zhivkov <zhivkovalexander697@gmail.com>
2025-05-21 21:10:12 +0300

    README.js
    Something interesting for me in the form of a code.

--- README.js
+++ README.js
@@ -1,14 +1,33 @@
 function aboutMe(somethingForMe) {
 - 👋 Hi, I’m @AlexRacing98
-- 👀 I’m interested in programming with Javascript 
-- 🌱 I’m currently learning Programming Basics with Javascript in SoftUni
-- 📫 How to reach me: zhivkovalexander697@gmail.com 
-   console.log(⚡Fun fact: Did you know that the first computer bug was literally a real bug? 🐜 In 1947, engineers working with Harvard's Mark II computer discovered that a moth had gotten between the relays, causing a malfunction. They removed the bug and recorded the incident as a "bug", which becomes a term that we still use today for software bugs!);
+- 👀 I’m interested in programming
+  with Javascript 
+- 🌱 I’m currently learning
+  Programming Basics with Javascript in SoftUni
+- 📫 How to reach me: 
+  zhivkovalexander697@gmail.com 
+   console.log(⚡Fun fact: Did you
+   know that the first computer bug
+   was literally a real bug? 🐜 
+   In 1947, engineers working with
+   Harvard's Mark II computer 
+   discovered that a moth had 
+   gotten between the relays, 
+   causing a malfunction. 
+   They removed the bug and 
+   recorded the incident as 
+   a "bug", which becomes a 
+   term that we still use today 
+   for software bugs!);
 } 
 
 aboutMe(😎);
 
 <!---
-AlexRacing98/AlexRacing98 is a ✨ special ✨ repository because its `README.js` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
+AlexRacing98/AlexRacing98 is a 
+✨ special ✨ repository because 
+its `README.js` (this file) appears
+on your GitHub profile. You can
+click the Preview link to
+take a look at your changes.
 --->
