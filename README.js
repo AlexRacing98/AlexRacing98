320a1932391500a68daed0695320736c27717f6d
Alexander Danielov Zhivkov <zhivkovalexander697@gmail.com>
2025-05-21 21:00:39 +0300

    README.js
    An interesting code with information about me.😎

---
+++ README.js
@@ -0,0 +1,14 @@
+function aboutMe(somethingForMe) {
+- 👋 Hi, I’m @AlexRacing98
+- 👀 I’m interested in programming with Javascript 
+- 🌱 I’m currently learning Programming Basics with Javascript in SoftUni
+- 📫 How to reach me: zhivkovalexander697@gmail.com 
+   console.log(⚡Fun fact: );
+} 
+
+aboutMe(😎);
+
+<!---
+AlexRacing98/AlexRacing98 is a ✨ special ✨ repository because its `README.js` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
